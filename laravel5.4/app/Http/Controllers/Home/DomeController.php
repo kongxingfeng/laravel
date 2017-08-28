517fb14c0f51e191a511b670404f91e788069a8a
zys01 <1031693105@qq.com>
2017-08-28 14:49:46 +0800

    添加demo控制器

---
+++ laravel5.4/app/Http/Controllers/Home/DomeController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 唯你
+ * Date: 2017/8/28
+ * Time: 14:47
+ */
+
+namespace App\Http\Controllers\Home;
+
+
+use Illuminate\Routing\Controller;
+
+class DomeController extends Controller {
+
+}
