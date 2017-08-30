c2b8b0c54009bd30e8aeac74e1a292e693eb7834
zhangying1228 <1021689788@qq.com>
2017-08-31 07:51:01 +0800

    556

--- laravel5.4/resources/views/adminlayout/adminmian.blade.php
+++ laravel5.4/resources/views/adminlayout/adminmian.blade.php
@@ -21,7 +21,7 @@
   <div class="leftnav-title"><strong><span class="icon-list"></span>菜单列表</strong></div>
   <h2><span class="icon-user"></span>用户设置</h2>
   <ul style="display:block">
-    <li><a href="/adminborrow"><span class="icon-caret-right"></span>用户审核</a></li>
+    <li><a href="/admin/adminborrow"><span class="icon-caret-right"></span>用户审核</a></li>
   </ul>   
   <h2><span class="icon-pencil-square-o"></span>栏目管理</h2>
   <ul>
