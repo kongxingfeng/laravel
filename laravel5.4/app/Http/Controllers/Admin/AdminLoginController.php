c072a20fee21bf5d2aba214e1fa0990bf07b4974
duanxinyu126 <2321833136@qq.com>
2017-08-31 08:14:44 +0800

    77778

---
+++ laravel5.4/app/Http/Controllers/Admin/AdminLoginController.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 唯你
+ * Date: 2017/8/27
+ * Time: 20:06
+ */
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Admin;
+
+class AdminLoginController extends Controller
+{
+
+	public function index()
+	{
+		return view('adminIndex/login');
+	}
+
+	public function adminadd(Request $request)
+            { 
+                    	
+                        $this->validate($request, [
+                            'name' => 'required',
+                            'password' => 'required|min:3|max:30',
+                            'is_remember' =>'integer',
+                        ]
+        );
+
+
+         $user = request(['name', 'password']);
+		
+
+
+
+        $remember = boolval(request('is_remember'));
+       
+        if (true == \Auth::guard('admin')->attempt($user)) {
+            return redirect('/admin/adminindex');
+        }
+		else
+		{
+			//return redirect('/adminindex');
+			return \Redirect::back()->withErrors("用户名密码错误");
+		}
+
+        //echo 1;
+       
+    }
+
+    /**
+     * 退出
+     */
+    public function adminlogout()
+    {
+        \Auth::guard('admin')->logout();
+        return redirect('/admin/adminlogin');
+    }
+}
---
+++ laravel5.4/app/Http/Controllers/Home/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 唯你
+ * Date: 2017/8/27
+ * Time: 20:06
+ */
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller{
+
+}
