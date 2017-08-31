947302a68090fdf5acac578e7354f798b5f1fbd6
zhangying1228 <1021689788@qq.com>
2017-08-31 09:19:12 +0800

    555

--- laravel5.4/app/Http/Controllers/Admin/AdminIndexController.php
+++ laravel5.4/app/Http/Controllers/Admin/AdminIndexController.php
@@ -15,16 +15,34 @@ class AdminIndexController extends Controller{
 	{
 		return view('adminIndex/index');
 	}
+	//审核借款人
 	public function borrow()
 	{
-		$data=DB::table('borrow')->get();
+		if(!empty($_GET))
+		{
+			//echo 11;
+			$type=$_GET['type'];
+			$id = $_GET['id'];
+			$arr=DB::table('borrow')->where('id',$id)->first();
+			if($type==0)
+			{
+			
+				$nowtime = date("Y-m-d H:i:s");//审核时间
+				$bor_etime=date("Y-m-d", strtotime("+".$arr->bor_month." months", strtotime($nowtime)));//还款时间
+				$info=DB::table('borrow')->where('id',$id)->update(['status'=>'1','bor_time'=>$nowtime,'bor_etime'=>$bor_etime]);
+				if($info)
+				{
+					echo $bor_etime;
+				}
+			}
+		}
+		else
+		{
+			$data=DB::table('borrow')->get();
+			return view('adminIndex/column',['data'=>$data]);
+		}
 		
-		return view('adminIndex/column',['data'=>$data]);
 	}
-	public function borrowad()
-	{
-		$type=$_GET['type'];
-		DB::table('borrowad');
-	}
+	
 
 }
